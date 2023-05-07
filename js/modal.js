dab2af8fe733f8366db4d9fa2f15705b512adada
Vitali Nesteruk <agent708@gmail.com>
2023-05-07 12:40:04 +0300

    fix: Corrected cod for madals in html and css. Remade modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,79 +1,40 @@
-let modal001 = document.getElementById("modal-001");
+const modal = document.querySelector('#modal');
+const buttonClose = document.querySelector('#modal-close');
+const btn001 = document.querySelector('#btn-001');
+content_1 = document.querySelector('.content-1');
+const btn002 = document.querySelector('#btn-002');
+content_2 = document.querySelector('.content-2');
+const btn003 = document.querySelector('#btn-003');
+content_3 = document.querySelector('.content-3');
 
-let btn001 = document.getElementById("btn-001");
+btn001.addEventListener('click', () => {
+    content_1.classList.add('active');
+    openModal(); 
+});
+btn002.addEventListener('click', () => {
+    content_2.classList.add('active');
+    openModal();   
+});
+btn003.addEventListener('click', () => {
+    content_3.classList.add('active');
+    openModal();
+});
 
-let btnClose001 = document.getElementById("btn-close-001");
-
-btn001.onclick = function() {
-    modal001.style.display = "block";
+const openModal = () => {
+    modal.classList.add('open');
 }
 
-btnClose001.onclick = function() {
-    modal001.style.display = "none";
+buttonClose.addEventListener('click', () => {
+    closeModal();
+    content_1.classList.remove('active');
+    content_2.classList.remove('active');
+    content_3.classList.remove('active');
+});
+
+const closeModal = () => {
+    modal.classList.remove('open');
 }
 
 document.addEventListener("keydown", function(e) {
-    if (modal001.style.display === "block") {
-        if(e.code === "Escape") {
-            modal001.style.display = "none";
-        }
-    } 
+    closeModal();
 });
-
-
-let modal002 = document.getElementById("modal-002");
-
-let btn002 = document.getElementById("btn-002");
-
-let btnClose002 = document.getElementById("btn-close-002");
-
-btn002.onclick = function() {
-    modal002.style.display = "block";
-}
-
-btnClose002.onclick = function() {
-    modal002.style.display = "none";
-}
-
-document.addEventListener("keydown", function(e) {
-    if (modal002.style.display === "block") {
-        if(e.code === "Escape") {
-            modal002.style.display = "none";
-        }
-    } 
-});
-
-let modal003 = document.getElementById("modal-003");
-
-let btn003 = document.getElementById("btn-003");
-
-let btnClose003 = document.getElementById("btn-close-003");
-
-btn003.onclick = function() {
-    modal003.style.display = "block";
-}
-
-btnClose003.onclick = function() {
-    modal003.style.display = "none";
-}
-
-document.addEventListener("keydown", function(e) {
-    if (modal003.style.display === "block") {
-        if(e.code === "Escape") {
-            modal003.style.display = "none";
-        }
-    } 
-});
-
-
-// let closeButton = document.querySelectorAll("btn-close");
-// closeButton.forEach (function(item){
-//     item.addEventListener("click", function(event){
-//         var modalId = this.getElementById('btn-close-003'),
-//         modalElement = document.querySelector('.modal[btn-close="' + modalId + '"]');
-//         modalElement.style.display = "none";
-//     });
-// });
-
-
-
